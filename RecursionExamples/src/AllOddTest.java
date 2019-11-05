2791cb6664563a9c4e6ddd70f029f407468f74e8
adityagupta <adityaofficialgupta@gmail.com>
2019-11-05 18:26:47 +0000

    added AllOddTest.java solution in recursion

---
+++ RecursionExamples/src/AllOddTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AllOddTest {
+	
+	public static boolean allOdds(int value) {
+
+		if(value%2==0)
+			return false;
+		if(value<10) 
+			return (value%2==1);
+	
+		return allOdds(value/10);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Value:");
+		int number =sc.nextInt();
+
+		System.out.println("Max= "+ allOdds(number));
+		sc.close();
+	}
+}
