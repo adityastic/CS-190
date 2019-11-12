3058159ad55976ccd69440f666a6bf196783c03e
a00268733 <a00268733@Z205-A15.studentnet.ait.ie>
2019-11-12 11:08:24 +0000

    added skeleton for linked list

---
+++ DataStructures/src/com/list/linked/LLNode.java
@@ -0,0 +1,19 @@
+package com.list.linked;
+class LLNode{
+	private String name;
+	private int age;
+	public  LLNode next;
+
+	public  String readname()
+                           {return name;}
+	public  int readage()
+                           {return age;}
+	public  LLNode(String nm,int a)
+	              {name = new String(nm);
+				   age=a;}
+	public String print(){
+		return "["+name+":"+age+"]-->";
+	
+	}
+}
+
