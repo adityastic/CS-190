d161b2b313459c7967f039927836c63f1194fbdc
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-01-21 14:26:42 +0000

    feat: added lock to threading-q8

--- Threading/src/threading/q7/ThreadUnsafe.java
+++ Threading/src/threading/q7/ThreadUnsafe.java
@@ -1,32 +1,37 @@
 package threading.q7;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class ThreadUnsafe extends Thread {
 	static int val1 = 9, val2 = 2, delay = 1000;
 
 	void Go(){
+		final Lock lock = new ReentrantLock();
+		lock.lock();
 		if (val2 != 0){
 			try {
 				Thread.sleep(delay);
 			}catch(Exception e){}
 			System.out.println( val1 / val2);
-			val2=0;
 		} else {
 			System.out.println( "Val2=0");
 		}
+		val2=0;
 	}
 	
 	public void run(){
-		    Go();
+		Go();
 	}
 
 	
     public static void main( String[] args ){
     	ThreadUnsafe t1=new ThreadUnsafe();
     	ThreadUnsafe t2=new ThreadUnsafe();
-    	val1=9; val2=2;
+    	val1=9;
+    	val2=5;
     	t1.start();
-    	delay=1;
+    	delay=1000;
     	t2.start();
     }
 }
