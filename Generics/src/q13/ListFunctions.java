65a786dc31123c6e0127479bd855d9b14ee21e41
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-02-26 10:41:03 +0000

    added q13 in generics

---
+++ Generics/src/q13/ListFunctions.java
@@ -0,0 +1,18 @@
+package q13;
+
+import java.util.Stack;
+
+class ListFunctions {
+
+    public static void printlist(Stack<Node> list) {
+        Node temp;
+        System.out.println("\nTOP\n");
+        int i = list.size() - 1;
+        while (i >= 0) {
+            temp = (Node) list.get(i);
+            i--;
+            temp.print();
+            System.out.println();
+        }
+    }
+}
